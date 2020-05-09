12cd533c0933214184e611070abddaadbdd0ec51
Paul Brown <pbLinuxLT@gmail.com>
2020-05-09 12:14:02 +0100

    firsStore.js

--- src/config_firestore.js
+++ src/config_firestore.js
@@ -2,6 +2,8 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';  
   
   // Your web app's Firebase configuration
+  // ** Important - rename this file to just firestore.js removing the config_
+
   var firebaseConfig = {
     apiKey: "",
     authDomain: "",
